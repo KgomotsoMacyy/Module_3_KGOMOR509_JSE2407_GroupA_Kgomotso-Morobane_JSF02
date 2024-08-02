332e2f60d5e9a9bab6e1a770549d27f1b6719c6d
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-08-02 12:59:24 +0200

    Fixed project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Router, Route } from "svelte-routing"
+  import Header from "./components/NavBar.svelte";
+  import Home from "./pages/Home.svelte";
+  import ProductPreview from "./pages/ProductPreview.svelte";
+</script>
+
+<Header />
+<Router>
+  <Route path="/" component={Home} />
+  <Route path="products/:id" component={ProductPreview} />
+</Router>
+ 
+<style></style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,11 @@
+<script>
+    import FilterSort from "../components/FilterSort.svelte";
+    import ProductsList from "../components/ProductsList.svelte";
+</script>
+
+<main class="bg-gray-300 text-gray-800">
+    <div>
+        <FilterSort />
+    </div>
+    <ProductsList />
+</main>
